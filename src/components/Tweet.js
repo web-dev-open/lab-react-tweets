c99562767f677ad4a0c071250823e6bf603467fd
james4-daf <d.james@elsevier.com>
2023-02-08 20:19:11 +0000

    initial set up

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,40 +1,40 @@
-function Tweet() {
-    return (
-      <div className="tweet">
-        <img
-          src="https://imgur.com/ebIbhBy.png"
-          className="profile"
-          alt="profile"
-        />
-  
-        <div className="body">
-          <div className="top">
-            <span className="user">
-              <span className="name">Root Learn</span>
-              <span className="handle">@root.learn</span>
-            </span>
-  
-            <span className="timestamp">Nov 30, 2022</span>
-          </div>
-  
-          <p className="message">
-            On December 7th, we will be hosting a #webinar that will introduce you
-            to #SQL! Are you ready? 🚀
-          </p>
-  
-          <div className="actions">
-            {/* Font Awesome icons */}
-            <i class="far fa-comment"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="far fa-heart"></i>
-            <i class="fas fa-share"></i>
-          </div>
+import ProfileImage from "./ProfileImage";
+
+function Tweet(props) {
+  const { tweet } = props;
+  const {
+    user: { name, image, handle },
+    timestamp,
+    message,
+  } = tweet;
+  return (
+    <div className="tweet">
+      <ProfileImage profileImage={image} />
+
+      <div className="body">
+        <div className="top">
+          <span className="user">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+          </span>
+
+          <span className="timestamp">{timestamp}</span>
         </div>
-  
-        <i class="fas fa-ellipsis-h"></i>
+
+        <p className="message">{message}</p>
+
+        <div className="actions">
+          {/* Font Awesome icons */}
+          <i class="far fa-comment"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+          <i class="fas fa-share"></i>
+        </div>
       </div>
-    );
-  }
-  
-  export default Tweet;
-  
+
+      <i class="fas fa-ellipsis-h"></i>
+    </div>
+  );
+}
+
+export default Tweet;
